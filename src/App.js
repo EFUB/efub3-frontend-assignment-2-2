6a8abd6ed53a4066a6640d883945b33c5683c8e5
Yerineee <yerin1103@ewhain.net>
2023-09-23 16:48:02 +0900

    :sparkles: 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import MyPage from "./pages/MyPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/my" element={<MyPage />} />
+    </Routes>
+  );
 }
 
 export default App;
